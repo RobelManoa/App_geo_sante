12686f493af0dd1dbbe34397f4c9adcbe2315cd7
Robel <manoarobel@gmail.com>
2025-06-14 16:12:25 +0300

    deployement vercel

--- MedicApp/src/api/api.ts
+++ MedicApp/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.88.237:5000/api",
+  baseURL: "https://appgeosante-production.up.railway.app/api",
   timeout: 10000,
 });
 
